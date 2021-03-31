1ac6f62094aafaa8489d51b377af51a5b15b08eb
Rob Jenkins <jenkinsr@mctc.edu>
2021-03-31 14:15:58 -0400

    Update

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Container, Text } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import MainScreen from './Screens/MainScreen';
+import SecondScreen from './Screens/SecondScreen';
+import CameraScreen from './Screens/CameraScreen';
+
+import { Container, Content, View, Text } from 'native-base';
+import { StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
+
+const styles = StyleSheet.create({
+  screenDefault: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#b0733e',
+  },
+  screenText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#fff',
+  }
+});
 
 export default class App extends React.Component {
   constructor(props) {
@@ -28,7 +47,28 @@ export default class App extends React.Component {
     }
 
     return (
-      <MainScreen />
+      <Container>
+        <Content>
+          <Swiper>
+
+            <View style={styles.screenDefault}>
+              <Text style={styles.screenText}>Main Screen</Text>
+            </View>
+
+            <CameraScreen />
+            
+            <View style={styles.screenDefault}>
+              <Text style={styles.screenText}>Screen Two</Text>
+            </View>
+            
+            <View style={styles.screenDefault}>
+              <Text style={styles.screenText}>Third Screen</Text>
+            </View>
+
+
+          </Swiper>
+        </Content>
+      </Container>
     );
   }
 }
